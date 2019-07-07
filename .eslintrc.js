e1b286f76859013b6c98fc5d918caadd4ebed92b
Martin <mr.mmaurer+work@gmail.com>
2019-07-08 00:31:48 +0200

    Fixed linter errors

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+  ],
+  "parser": "babel-eslint",
+  "rules": {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+        ]
+      }
+    ]
+  },
+}
--- src/App.js
+++ src/App.js
@@ -17,58 +17,63 @@ function App() {
         if (response.ok) {
           return response.json();
         }
+
+        throw Error(response.statusText);
       })
       .then((data) => {
-        const { results } = data
+        const { results } = data;
 
         setPersonsList(results);
         setIsFetching(false);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-  }
+  };
 
   const handleChange = (event) => {
-    const value = event.target.value;
+    const { value } = event.target;
 
     setInputValue(value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     fetchUserData();
-  }
+  };
 
   return (
-    <div className={ style.App }>
-      <h1 className={ style.title }>Search</h1>
+    <div className={style.App}>
+      <h1 className={style.title}>Search</h1>
 
       <form
-        className={ style.form }
-        onSubmit={ handleSubmit }
+        className={style.form}
+        onSubmit={handleSubmit}
         action="post"
         method="post"
         autoComplete="off"
       >
-        <fieldset className={ style.formRow }>
+        <fieldset className={style.formRow}>
           <input
-            className={ style.field }
-            value={ inputValue }
-            onChange={ handleChange }
+            className={style.field}
+            value={inputValue}
+            onChange={handleChange}
             type="search"
             autoComplete="false"
             placeholder="Please enter number of entries to show"
-            disabled={ isFetching ? 'disabled': false }
+            disabled={isFetching ? 'disabled' : false}
           />
-          <button className={ style.button } type="submit">Search</button>
+          <button className={style.button} type="submit">Search</button>
         </fieldset>
         { isFetching === true && <span>Loading!!!</span> }
         { (personsList.length > 0) && (
-          <ul className={ style.suggestions }>
-            { personsList.map((person, index) => (
-              <li className={ style.suggestion } key={ index }>
-                <span className={ style.suggestionEntry }>{ person.name.first }</span>
-                <span className={ style.suggestionEntry }>{ person.name.last }</span>
-                <span className={ style.suggestionEntry }>{ person.email }</span>
+          <ul className={style.suggestions}>
+            { personsList.map(person => (
+              <li className={style.suggestion} key={person.id}>
+                <span className={style.suggestionEntry}>{ person.name.first }</span>
+                <span className={style.suggestionEntry}>{ person.name.last }</span>
+                <span className={style.suggestionEntry}>{ person.email }</span>
               </li>
             ))}
           </ul>
