f63645fcc50828edb07ee9678427ed51787ae205
Martin <mr.mmaurer+work@gmail.com>
2019-07-07 21:39:36 +0200

    Added basic html layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div className="app">
-
+    <div className={ style.App }>
+      <form className={ style.form } action="post" method="post" autocomplete="off">
+        <h1 className={ style.title }>Person search</h1>
+        <fieldset className={ style.formRow }>
+          <input className={ style.field } type="search" autocomplete="false" value="" />
+          <button className={ style.button } type="submit">Search</button>
+        </fieldset>
+        <ul className={ style.suggestions }>
+          <li className={ style.suggestion }>Test</li>
+        </ul>
+      </form>
     </div>
   );
 }
